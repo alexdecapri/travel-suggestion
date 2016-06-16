2b84a54fb17cf9483a19ab50d405ea14692f239f
alexdecapri <alexdecapri@gmail.com>
2016-06-16 16:24:02 -0600

    saving changes

--- views/questions/questionCtrl.js
+++ views/questions/questionCtrl.js
@@ -2,11 +2,13 @@ var app = angular.module('travelApp');
 
 app.controller('questionCtrl', function($scope, $rootScope, questionService) {
 
+  // $scope.getCountry = questionService.getCountry($root.q1Answer, $root.q2Answer, $root.q3Answer) {
+  //
+  // }
 
-
-  $scope.testAnswer = function() {
-    alert("test");
-    console.log($scope.q1Answer);
-  }
+  // $scope.testAnswer = function() {
+  //   alert("test");
+  //   console.log($scope.q1Answer);
+  // }
 
 });
