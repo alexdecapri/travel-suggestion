dddfca0aa0913958b2cf026fe8847e26cac235a2
alexdecapri <alexdecapri@gmail.com>
2016-06-16 15:36:45 -0600

    added list of example countries in americas region

--- views/questions/questionService.js
+++ views/questions/questionService.js
@@ -2,4 +2,39 @@ var app = angular.module('travelApp');
 
 app.service('questionService', function() {
 
+  var countries = [
+    {
+      name: 'United States',
+      region: 'americas',
+      budget: 'medium',
+      type: 'geography'
+    },
+    {
+      name: 'Canada',
+      region: 'americas',
+      budget: 'high',
+      type: 'food'
+    },
+    {
+      name: 'Brazil',
+      region: 'americas',
+      budget: 'medium',
+      type: 'food'
+    },
+    {
+      name: 'Argentina',
+      region: 'americas',
+      budget: 'low',
+      type: 'geography'
+    },
+    {
+      name: 'Panama',
+      region: 'americas',
+      budget: 'low',
+      type: 'history'
+    }
+  ];
+
+  //REST countries API https://restcountries.eu/
+
 });
