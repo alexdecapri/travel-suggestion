86a7ab7527715d84afe8859b814a9ab349d2f3ad
alexdecapri <alexdecapri@gmail.com>
2016-06-16 15:24:19 -0600

    jquery file muted, js added to each controller

--- js/jquery.js
+++ js/jquery.js
@@ -1,26 +1,26 @@
-var colors = ['#FF4D63', '#3848AC', '#513584', '#9D4075', '#4D7AFF', '#875EEF', '#525561'];
-
-$(document).ready(function() {
-
-  getRandomColor();
-  $('button').click(getRandomColor());
-
-  function getRandomColor() {
-
-    var color = colors[Math.floor(Math.random() * colors.length)];
-
-    $('body').animate({
-        backgroundColor: color,
-        color: color
-    }, 800);
-    $('#main-box').animate({
-        color: color
-    }, 800);
-    $('#top-banner').animate({
-        color: color
-    }, 800);
-
-
-  }
-
-});
+// var colors = ['#FF4D63', '#3848AC', '#513584', '#9D4075', '#4D7AFF', '#875EEF', '#525561'];
+//
+// $(document).ready(function() {
+//
+//   getRandomColor();
+//   $('button').click(getRandomColor());
+//
+//   function getRandomColor() {
+//
+//     var color = colors[Math.floor(Math.random() * colors.length)];
+//
+//     $('body').animate({
+//         backgroundColor: color,
+//         color: color
+//     }, 800);
+//     $('#main-box').animate({
+//         color: color
+//     }, 800);
+//     $('#top-banner').animate({
+//         color: color
+//     }, 800);
+//
+//
+//   }
+//
+// });
--- views/questions/questionCtrl.js
+++ views/questions/questionCtrl.js
@@ -8,4 +8,31 @@ app.controller('questionCtrl', function($scope, questionService) {
     console.log($scope.q1Answer);
   }
 
+  var colors = ['#FF4D63', '#3848AC', '#513584', '#9D4075', '#4D7AFF', '#875EEF', '#525561'];
+
+  $(document).ready(function() {
+
+    $('button').click(getRandomColor());
+
+    function getRandomColor() {
+
+      var color = colors[Math.floor(Math.random() * colors.length)];
+
+      $('body').animate({
+          backgroundColor: color,
+          color: color
+      }, 800);
+      $('#main-box').animate({
+          color: color
+      }, 800);
+      $('#top-banner').animate({
+          color: color
+      }, 800);
+
+
+    }
+
+  });
+
+
 });
