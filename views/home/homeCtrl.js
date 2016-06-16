1bb8a95e22956b1b35602e10d46f27b2a8519702
alexdecapri <alexdecapri@gmail.com>
2016-06-16 13:59:23 -0600

    tried randomized color but isn't working yet, also adding minor updates

--- views/home/homeCtrl.js
+++ views/home/homeCtrl.js
@@ -2,4 +2,28 @@ var app = angular.module('travelApp');
 
 app.controller('homeCtrl', function($scope) {
 
+  var colors = ['#FF4D63', '#3848AC', '#513584', '#9D4075', '#4D7AFF', '#875EEF', '#525561'];
+
+  $(document).ready(function() {
+
+    getRandomColor();
+    // $('button').on('click', getRandomColor());
+
+    function getRandomColor() {
+
+      var color = Math.floor(Math.random() * colors.length);
+
+      $('body').animate({
+          backgroundColor: colors[color],
+          color: colors[color]
+      }, 800);
+      $('#main-box').animate({
+          color: colors[color]
+      }, 800);
+
+
+    }
+
+  });
+
 });
