df3f6a8d411ac3d0fe768c5ac883164b8fb1c7b8
alexdecapri <alexdecapri@gmail.com>
2016-06-18 17:25:59 -0600

    making some progress with displaying country stats

--- views/final/finalCtrl.js
+++ views/final/finalCtrl.js
@@ -1,12 +1,33 @@
 var app = angular.module('travelApp');
 
-app.controller('finalCtrl', function($scope, $rootScope, questionService) {
+app.controller('finalCtrl', function($scope, $rootScope, questionService, finalService) {
 
   // console.log($rootScope.q1Answer, $rootScope.q2Answer, $rootScope.q3Answer);
 
   var selectCountries = questionService.buildCountries($rootScope.q1Answer, $rootScope.q2Answer, $rootScope.q3Answer);
   $scope.countryToDisplay = selectCountries[Math.floor(Math.random() * selectCountries.length)].name;
-  console.log($scope.countryToDisplay);
+  var name = selectCountries[Math.floor(Math.random() * selectCountries.length)].name;
+  console.log('countryToDisplay:', $scope.countryToDisplay);
+
+  $scope.fetchInfo = function() {
+    finalService.getCountryInfo(name)
+    .then(function(response) {
+      console.log('fetchInfo response:', response);
+      $scope.details = response;
+    }).catch(function(err) {
+      console.log(err);
+    });
+  }
+  $scope.fetchInfo();
+
+  console.log('info', $scope.details);
+
+
+
+  // $scope.fetchInfo = finalService.getCountryInfo($scope.countryToDisplay);
+
+  // console.log($scope.fetchInfo);
+
 
 
 });
--- views/final/finalService.js
+++ views/final/finalService.js
@@ -1,5 +1,21 @@
 var app = angular.module('travelApp');
 
-app.service('finalService', function() {
+app.service('finalService', function($http, $q) {
+
+  this.getCountryInfo = function(name) {
+    console.log('NAME IN SERVICE', name);
+    var dfd = $q.defer();
+    $http({
+      method: 'GET',
+      url: 'http://restcountries.eu/rest/v1/name/' + name,
+    }).then(function(response) {
+      dfd.resolve(response);
+      // console.log(response.data);
+    }).catch(function(err) {
+      console.error('Error message:', err);
+      dfd.reject(err);
+    })
+    return dfd.promise;
+  }
 
 });
