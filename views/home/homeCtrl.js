e554c4e7a12402d843799f38934a1e5464ff7fe0
alexdecapri <alexdecapri@gmail.com>
2016-06-21 17:50:52 -0600

    body starting to change color

--- views/home/homeCtrl.js
+++ views/home/homeCtrl.js
@@ -3,28 +3,42 @@ var app = angular.module('travelApp');
 app.controller('homeCtrl', function($scope, $rootScope) {
 
   var colors = ['#FF4D63', '#3848AC', '#513584', '#9D4075', '#4D7AFF', '#875EEF', '#525561'];
+  var randomColor = colors[Math.floor(Math.random() * colors.length)];
+  var pageColor = document.body;
+  console.log(pageColor);
 
-  $(document).ready(function() {
-    // console.log("working?");
+  var changeColor = function() {
+    console.log(randomColor);
+    pageColor.style.backgroundColor = randomColor;
+    console.log("got here");
+  };
 
-    getRandomColor();
-    // $('button').on('click', getRandomColor());
-
-    function getRandomColor() {
-
-      var color = Math.floor(Math.random() * colors.length);
-
-      $('body').animate({
-          backgroundColor: colors[color],
-          color: colors[color]
-      }, 800);
-      $('#main-box').animate({
-          color: colors[color]
-      }, 800);
+  changeColor();
 
 
-    }
 
-  });
+
+  // $(document).ready(function() {
+  //   // console.log("working?");
+  //
+  //   getRandomColor();
+  //   // $('button').on('click', getRandomColor());
+  //
+  //   function getRandomColor() {
+  //
+  //     var color = Math.floor(Math.random() * colors.length);
+  //
+  //     $('body').animate({
+  //         backgroundColor: colors[color],
+  //         color: colors[color]
+  //     }, 800);
+  //     $('#main-box').animate({
+  //         color: colors[color]
+  //     }, 800);
+  //
+  //
+  //   }
+  //
+  // });
 
 });
