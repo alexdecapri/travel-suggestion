7df5b362d7e22e568857e8ec86eb6cf64cbf897b
alexdecapri <alexdecapri@gmail.com>
2016-06-22 15:22:46 -0600

    bootstrap applied to buttons, working on button color change

--- js/colorService.js
+++ js/colorService.js
@@ -5,9 +5,11 @@ app.service('colorService', function() {
   this.changeColor = function() {
     var colors = ['#FF4D63', '#3848AC', '#513584', '#9D4075', '#4D7AFF', '#875EEF', '#525561'];
     var randomColor = colors[Math.floor(Math.random() * colors.length)];
-    var pageColor = document.body;
-    pageColor.style.backgroundColor = randomColor;
-    pageColor.style.color = randomColor;
+    var page = document.body;
+    var btn = document.getElementsByClassName('btn-primary');
+    page.style.backgroundColor = randomColor;
+    page.style.color = randomColor;
+    document.getElementsByClassName("btn btn-primary").style.backgroundColor = randomColor;
   }
 
 });
