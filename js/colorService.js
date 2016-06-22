a6b0ca1e59e335f985a4f184864af4c1fccba59a
alexdecapri <alexdecapri@gmail.com>
2016-06-22 16:15:13 -0600

    buttons fixed

--- js/colorService.js
+++ js/colorService.js
@@ -6,11 +6,15 @@ app.service('colorService', function() {
     var colors = ['#FF4D63', '#3848AC', '#513584', '#9D4075', '#4D7AFF', '#875EEF', '#525561'];
     var randomColor = colors[Math.floor(Math.random() * colors.length)];
     var page = document.body;
-    var btn = document.getElementsByClassName('btn-primary');
+    var btns = document.getElementsByClassName('btn-primary');
     page.style.backgroundColor = randomColor;
     page.style.color = randomColor;
-    console.log(btn);
-    // document.getElementsByClassName("btn btn-primary").style.backgroundColor = randomColor;
+
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].style.backgroundColor = randomColor;
+      btns[i].style.borderColor = randomColor;
+    }
+
   }
 
 });
