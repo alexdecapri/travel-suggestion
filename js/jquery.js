6644073c4d088f5579d43734c64feb497476cf96
alexdecapri <alexdecapri@gmail.com>
2016-06-22 14:46:58 -0600

    transitions working on click, button still not working

--- js/jquery.js
+++ js/jquery.js
@@ -1,21 +1,23 @@
   var colors = ['#FF4D63', '#3848AC', '#513584', '#9D4075', '#4D7AFF', '#875EEF', '#525561'];
-  var randomColor = colors[Math.floor(Math.random() * colors.length)];
 
 $(document).ready(function() {
 
   changeColor();
-  $('button').on('click', changeColor);
+  $('body').on('click', changeColor);
 
   function changeColor() {
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+
     $('body').animate({
         backgroundColor: randomColor,
         color: randomColor
     }, 800);
-    $('button').animate({
+    $('.btn-primary').animate({
         backgroundColor: randomColor,
+        color: randomColor
     }, 800);
-    console.log("test");
   }
+
 
 
 
