0e796d7a1aac4c0e822612144282f0db57d42c7a
alexdecapri <alexdecapri@gmail.com>
2016-06-24 13:55:54 -0600

    got rid of console.logs

--- views/final/finalService.js
+++ views/final/finalService.js
@@ -3,15 +3,15 @@ var app = angular.module('travelApp');
 app.service('finalService', function($http, $q) {
 
   this.getCountryInfo = function(name) {
-    console.log('NAME IN SERVICE', name);
+    // console.log('NAME IN SERVICE', name);
     var dfd = $q.defer();
     $http({
       method: 'GET',
       url: 'https://restcountries.eu/rest/v1/name/' + name,
     }).then(function(response) {
       dfd.resolve(response);
-      console.log(response.data[0].name);
-      console.log(response.data);
+      // console.log(response.data[0].name);
+      // console.log(response.data);
     }).catch(function(err) {
       console.error('Error message:', err);
       dfd.reject(err);
